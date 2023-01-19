3565b429203551e765209877d5e0d9d962820426
sarahhcheng <sarahcheng40@gmail.com>
2023-01-19 00:25:54 -0800

    initialize

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header">
+        <div className="flex-container">
+          <div className="title">Druber</div>
+          <div>
+            <ul class="druber-list">
+              <li>Company</li>
+              <li>Safety</li>
+              <li>Help</li>
+            </ul>
+          </div>
+        </div>
       </header>
-    </div>
+    </>
   );
-}
-
+};
 export default App;
