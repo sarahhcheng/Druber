e3817c41e77c659549b9773a21e1c55be899826d
sarahhcheng <sarahcheng40@gmail.com>
2023-01-19 13:23:01 -0800

    added image

--- src/App.js
+++ src/App.js
@@ -16,6 +16,18 @@ const App = () => {
           </div>
         </div>
       </header>
+      <div className="phrase">Get in the pilot's seat and get paid</div>
+      <div className="container">
+        <div className="info-card">
+          <div class="text">
+            <div className="card-title"> Start your journey now.</div>
+            <div className="image">
+              <img src={require("./airplane.jpg")} alt="test"></img>
+            </div>
+            <p> Visit our website for more contents </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
